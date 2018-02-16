80a12e56f752af57ee1964e793475a1dd8ca56d2
ftmanspile <codfan15@gmail.com>
2018-02-16 11:01:54 -0500

    dropdown not working

--- app.rb
+++ app.rb
@@ -2,5 +2,24 @@ require "sinatra"
 require_relative "tic_tac.rb"
 
 get '/' do
+	winner_result = " "
 	erb :home
 end
+
+post '/board' do
+	space1 = params[:space1]
+	space2 = params[:space2]
+	space3 = params[:space3]
+	puts space1
+	redirect '/play?space1=' + space1 + '&space2=' + space2 + '&space3=' + space3
+end
+
+get '/play' do
+	winner_result = " "
+	space1 = params[:space1]
+	space2 = params[:space2]
+	space3 = params[:space3]
+	print space1	
+	erb :play, :locals => {:space1 => space1, :space2 => space2, :space3 => space3}
+end
+
--- tic_tac.rb
+++ tic_tac.rb
@@ -1,3 +1,18 @@
-class Tic_tac
-
-	
+def  winner(a,b,c,d,e,f,g,h,i)
+			x_wins = ["X","X","X"]
+			o_wins = ["O","O","O"]
+			argue_hash = {1 => a, 2 => b, 3 => c, 4 => d, 5 => e, 6 => f, 7 => g, 8 => h, 9 => i}
+			win_combos = [[argue_hash[1], argue_hash[2], argue_hash[3]], [argue_hash[3], argue_hash[6], argue_hash[9]], [argue_hash[7], argue_hash[8], argue_hash[9]], [argue_hash[1], argue_hash[4], argue_hash[7]], [argue_hash[1], argue_hash[5], argue_hash[9]], [argue_hash[3], argue_hash[5], argue_hash[7]], [argue_hash[2], argue_hash[5], argue_hash[8]], [argue_hash[4], argue_hash[5], argue_hash[6]]]
+		(0..7).each do |i|
+			if plays == x_wins
+				$result = "X's have won"
+				break
+			elsif plays == o_wins
+				$result = "The O's have won. Please try again"  
+				break
+			else
+				$result = "No winner yet, keep playing"
+			end
+		end
+	return $result #the reult is then sent back to the app and out to the webpage	
+end
